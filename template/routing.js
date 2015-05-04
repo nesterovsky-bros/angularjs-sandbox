c950e4f2538841bf79142a99747bdf7a3f962d7e
Vladimir Nesterovsky <vladimir@nesterovsky-bros.com>
2015-05-04 09:50:38 +0300

    Create routing.js

---
+++ template/routing.js
@@ -0,0 +1,19 @@
+angular.module("app").config(
+[
+  "$routeProvider",
+  function($routeProvider)
+  {
+    $routeProvider.
+      when(
+     	  "page1",
+     	  {
+          templateUrl: "page1.html"
+    	  }).
+      when(
+     	  "page2",
+     	  {
+          templateUrl: "page2.html"
+    	  }).
+      otherwise("page1");
+  }
+]);
